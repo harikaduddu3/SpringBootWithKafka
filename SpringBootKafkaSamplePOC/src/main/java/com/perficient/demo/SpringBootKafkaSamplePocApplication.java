393ecf5e8714efcc966e52316679be1975c3c6e3
harika.duddu <harikaduddu3@gmail.com>
2023-06-22 12:15:41 +0530

    spring boot with kafka

--- SpringBootKafkaSamplePOC/src/main/java/com/perficient/demo/SpringBootKafkaSamplePocApplication.java
+++ SpringBootKafkaSamplePOC/src/main/java/com/perficient/demo/SpringBootKafkaSamplePocApplication.java
@@ -2,8 +2,12 @@ package com.perficient.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan({"com.perficient.demo.controller","com.perficient.demo.utils","com.perficient.demo.service"})
+@Configuration
 public class SpringBootKafkaSamplePocApplication {
 
 	public static void main(String[] args) {
